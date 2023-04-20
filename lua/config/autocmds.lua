f91956c12ff2b42e4ccbd276b8fe96e641a57006
weakish <weakish@gmail.com>
2023-04-20 10:40:02 +0000

    :new: ltex-ls

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -24,3 +24,13 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
     ]])
   end,
 })
+
+-- Disable built-in spellchecking for Markdown, since LanguageTool is used.
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("lazyvim_user_markdown", { clear = true }),
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+  end,
+})

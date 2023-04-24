12855c18d102f0975627a6be2f6683b6a2126bdf
weakish <weakish@gmail.com>
2023-04-24 14:12:56 +0000

    :bug: disable denols for ts files not under Deno projects

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,9 @@ return {
     opts = {
       servers = {
         marksman = {}, -- markdown
+        denols = {
+          root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
+        }, -- Deno
       },
     },
     {
